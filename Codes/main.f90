85a1b9b35990a2504d6fbc71e553feec22a2c918
driyanti <dwiriyanti@yahoo.com>
2021-09-14 15:11:57 +0200

    Update main.f90

--- Codes/main.f90
+++ Codes/main.f90
@@ -1,2 +1,30 @@
 program main 
 implicit none
+
+!finalval.o\
+!resultval.o\
+!         solveqn.o\
+!         BHmxv.o\
+!         Gmxm.o\
+!         BGmxm.o\
+!         Hpmxv.o\
+!         Gfree.o\
+!         Gfree_polarz.o\
+!         Greentens.o\
+!         MxMGreen.o\
+!         matgreen.o\
+!         kxkyvalue.o\
+!         mksource.o \
+!         pmksource.o \
+!         mkpdom.o \
+!         mkmatD.o \
+!         mkmatDh.o\
+!         Besselfunc.o\
+!         mkmatE.o readpar.o initial.o zgemm.o\
+!         zgeco.o\
+!	       zgesl.o\
+!         zgemv.o xerbla.o lsame.o matproloog.o calcfield.o Gcalcfield.o initmsg.o files.o getpar.o
+
+return
+end
+       
