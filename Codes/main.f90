1996d9c21741372cf3ff2ba4f275fcd47aeaa571
driyanti <dwiriyanti@yahoo.com>
2021-09-14 15:15:07 +0200

    Update main.f90

--- Codes/main.f90
+++ Codes/main.f90
@@ -1,29 +1,33 @@
 program main 
 implicit none
 
-!finalval.o\
-!resultval.o\
-!         solveqn.o\
-!         BHmxv.o\
-!         Gmxm.o\
-!         BGmxm.o\
-!         Hpmxv.o\
-!         Gfree.o\
-!         Gfree_polarz.o\
-!         Greentens.o\
-!         MxMGreen.o\
-!         matgreen.o\
-!         kxkyvalue.o\
-!         mksource.o \
-!         pmksource.o \
-!         mkpdom.o \
-!         mkmatD.o \
-!         mkmatDh.o\
-!         Besselfunc.o\
-!         mkmatE.o readpar.o initial.o zgemm.o\
-!         zgeco.o\
-!	       zgesl.o\
-!         zgemv.o xerbla.o lsame.o matproloog.o calcfield.o Gcalcfield.o initmsg.o files.o getpar.o
+!finalval.f\
+!resultval.f\
+!         solveqn.f\
+!         BHmxv.f\
+!         Gmxm.f\
+!         BGmxm.f\
+!         Hpmxv.f\
+!         Gfree.f\
+!         Gfree_polarz.f\
+!         Greentens.f\
+!         MxMGreen.f\
+!         matgreen.f\
+!         kxkyvalue.f\
+!         mksource.f \
+!         pmksource.f \
+!         mkpdom.f \
+!         mkmatD.f \
+!         mkmatDh.f\
+!         Besselfunc.f\
+!         mkmatE.f \ 
+!         readpar.f \
+!         initial.f\
+!         zgemm.f\
+!         zgeco.f\
+!	   zgesl.f\
+!         zgemv.f \
+!         xerbla.f lsame.f matproloog.f calcfield.f Gcalcfield.f initmsg.f files.f getpar.f
 
 return
 end
